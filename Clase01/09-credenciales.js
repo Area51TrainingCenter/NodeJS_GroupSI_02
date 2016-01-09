0047952816052afbee3ca9a3aa1de95ccab10d90
Sergio <Sergiohidalgocaceres@gmail.com>
2016-01-08 22:05:30 -0500

    Clase01

---
+++ Clase01/09-credenciales.js
@@ -0,0 +1,13 @@
+var credenciales = require("./credenciales");
+
+console.log("Credenciales Facebook");
+console.log("=====================");
+console.log("Clave Servidor = " + credenciales.facebook.claveServidor);
+console.log("Clave Cliente = " + credenciales.facebook.claveCliente);
+console.log("Clave Callback = " + credenciales.facebook.callback);
+console.log("=====================");
+console.log("Credenciales Twitter");
+console.log("=====================");
+console.log("Clave Servidor = " + credenciales.twitter.claveServidor);
+console.log("Clave Cliente = " + credenciales.twitter.claveCliente);
+console.log("Clave Callback = " + credenciales.twitter.callback);
---
+++ Clase01/credenciales.js
@@ -0,0 +1,19 @@
+var credenciales = {
+	facebook: {
+		claveServidor: "8483848484",
+		claveCliente: "akdkdk3kckckc",
+		callback: "http://midominio/facebook/callback"
+	},
+	twitter: {
+		claveServidor: "afdfas",
+		claveCliente: "fasdfasdfc",
+		callback: "http://midominio/twitter/callback"
+	},
+	google: {
+		claveServidor: "24545",
+		claveCliente: "asdf",
+		callback: "http://midominio/google/callback"
+	}		
+};
+
+module.exports = credenciales;
