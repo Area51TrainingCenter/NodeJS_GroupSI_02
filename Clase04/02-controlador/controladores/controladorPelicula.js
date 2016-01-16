26bd0d475440137f5cc40acf1b61fcefa1c6db00
Alumno <Alumno>
2016-01-15 22:03:57 -0500

    Clase04

--- Clase04/02-controlador/controladores/controladorPelicula.js
+++ Clase04/02-controlador/controladores/controladorPelicula.js
@@ -15,9 +15,38 @@ controlador.listar = function(req, res, next){
 	});
 }
 
-controlador.insertar = function(req, res, next){}
+controlador.insertar = function(req, res, next){
+	var titulo = req.body.titulo,
+		director = req.body.director,
+		anno = req.body.anno;
 
-controlador.eliminar = function(req, res, next){}
+	var registro = {
+		titulo: titulo,
+		director: director,
+		anno: anno
+	}
+
+	modelo.insertar(registro, function(err){
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/");
+		}
+	})
+
+}
+
+controlador.eliminar = function(req, res, next){
+	var id = req.params.id;
+
+	modelo.eliminar(id, function(err){
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/");
+		}
+	})
+}
 
 controlador.editar = function(req, res, next){
 	var id = req.params.id;	
@@ -57,6 +86,8 @@ controlador.actualizar = function(req, res, next){
 
 }
 
-controlador.formInsertar = function(req, res, next){}
+controlador.formInsertar = function(req, res, next){
+	res.render("formInsertar");
+}
 
 module.exports = controlador
