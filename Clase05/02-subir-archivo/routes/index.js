39c3a4360055a5d77539e2e058b9b49c8ae695af
Alumno <Alumno>
2016-01-18 22:00:18 -0500

    Clase05

--- Clase05/02-subir-archivo/routes/index.js
+++ Clase05/02-subir-archivo/routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require("formidable");
+var fsExtra = require("fs-extra");
+var util = require("util");
+
+function fnSubir(req, res, next) {
+
+	function fnRecibidos(err, campos, archivos) {
+		res.send("Archivos recibidos: " + util.inspect({files: archivos}));
+	}
+
+	function fnAvance(recibidos, total) {
+		console.log("Avance: " + recibidos/total*100);
+	}	
+
+	function fnError(err) {
+		console.log(err);
+	}	
+
+	function fnFin(campos, archivos) {
+		var rutaTemporal = this.openedFiles[0].path,
+			nombreArchivo = this.openedFiles[0].name,
+			nuevoDirectorio = "./public/uploads/";
+
+		function fnCopiado(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				//res.redirect("/");
+			}
+		}
+
+		fsExtra.copy(rutaTemporal, nuevoDirectorio + nombreArchivo, fnCopiado)			
+	}
+			
+
+	var form = formidable.IncomingForm();
+
+	form.parse(req, fnRecibidos);
+	form.on("progress", fnAvance);
+	form.on("error", fnError);
+	form.on("end", fnFin);
+
+}
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/subir", fnSubir);
 
 module.exports = router;
